60124c057bdb102f1021b6a2ab9d6f84966d8cb7
chhatrodiya ravi <trendcart04@gmail.com>
2026-04-18 17:27:51 +0530

    add image scrolltrigger

--- src/components/Home/Video.jsx
+++ src/components/Home/Video.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Video = () => {
   return (
-    <div className='h-full w-full'>
-      <video  muted autoPlay loop
-      className='h-full w-full object-cover'
-      src="https://download-video-ak.vimeocdn.com/v3-1/playback/36bc59b8-6671-4358-abc2-15555fc6ae59/69496b2d?__token__=st=1776423300~exp=1776426900~acl=%2Fv3-1%2Fplayback%2F36bc59b8-6671-4358-abc2-15555fc6ae59%2F69496b2d%2A~hmac=60718b001e2a742d6999158791dc7f20741b85dbcbffebf4e01260001ab86385&r=dXMtZWFzdDE%3D"></video>
+    <div className="h-full w-full">
+      <video
+        muted
+        autoPlay
+        loop
+        className="h-full w-full object-cover"
+        src="/video/bgvideo.mp4"
+      ></video>
     </div>
-  )
-}
+  );
+};
 
-export default Video
+export default Video;
--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -1,11 +1,56 @@
-import React from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import React, { useRef } from "react";
+
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 const Agence = () => {
+  //reference for elemt
+  const imageDivRef = useRef(null);
+  const imageRef = useRef(null);
+
+  const imgArray = [
+    "https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7",
+    "https://k72.ca/images/teamMembers/Olivier_480x640.jpg?w=480&h=640&fit=crop&s=c13569c0753117d04f1a93cf7b446d64",
+    "https://k72.ca/images/teamMembers/Michele_480X640.jpg?w=480&h=640&fit=crop&s=ce85dc6d140947736baa739d0e59dab2",
+    "https://k72.ca/images/teamMembers/MEGGIE_480X640_2.jpg?w=480&h=640&fit=crop&s=3604b19f8fc7b40f517954147698d847",
+    "https://k72.ca/images/teamMembers/joel_480X640_3.jpg?w=480&h=640&fit=crop&s=1cadbf143b3aa916b1b414464acbb4d6",
+    "https://images.unsplash.com/photo-1761839258513-099c3121d72d?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyMnx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1773332611528-566f16120979?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0M3x8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1776345013776-98646ac3332d?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw4MHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1773332611573-5e5bfa8e5de5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxODN8fHxlbnwwfHx8fHw%3D",
+  ];
+
+  useGSAP(function () {
+    gsap.to(imageDivRef.current, {
+      // y: 450,
+      ease: "none",
+      scrollTrigger: {
+        trigger: imageDivRef.current,
+        markers: true,
+        start: "top 28%",
+        end: "top -100%",
+        scrub: true,
+        pin: true,
+        onUpdate: function (elem) {
+          // console.log("hurrayy!!!");
+          const imageIndex = Math.floor(elem.progress * imgArray.length);
+          imageRef.current.src = imgArray[imageIndex];
+        },
+      },
+    });
+  });
+
   return (
     <div>
       <div className="section1">
-        <div className="fixed h-[20vw] w-[15vw] rounded-3xl overflow-hidden   top-40 left-[30vw]">
+        <div
+          ref={imageDivRef}
+          className="absolute h-[20vw] w-[15vw] rounded-3xl overflow-hidden   top-40 left-[30vw]"
+        >
           <img
+            ref={imageRef}
             className="h-full w-full object-cover"
             src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7"
             alt=""
@@ -20,21 +65,18 @@ const Agence = () => {
             </h1>
           </div>
           <div className="pl-[40%] mt-20">
-            <p className=" text-6xl">
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              We’re inquisitive and open-minded, and we make sure creativity
-              crowds out ego from every corner. A brand is a living thing, with
-              values, a personality and a story. If we ignore that, we can
-              achieve short-term success, but not influence that goes the
-              distance. We bring that perspective to every brand story we help
-              tell.
+            <p className=" text-4xl">
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; We’re
+              inquisitive and open-minded, and we make sure creativity crowds
+              out ego from every corner. A brand is a living thing, with values,
+              a personality and a story. If we ignore that, we can achieve
+              short-term success, but not influence that goes the distance. We
+              bring that perspective to every brand story we help tell.
             </p>
           </div>
         </div>
       </div>
-      <div className="section2">
-
-      </div>
+      <div className="section2"></div>
     </div>
   );
 };
